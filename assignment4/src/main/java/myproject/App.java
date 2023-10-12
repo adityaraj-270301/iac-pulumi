45f229cc6abe6ce4e9e5874b223192598da7835b
adityaraj-2703 <raj.adi@northeastern.edu>
2023-10-12 12:15:52 -0400

    commiting assignment4

---
+++ assignment4/src/main/java/myproject/App.java
@@ -0,0 +1,15 @@
+package assignment4.src.main.java.myproject;
+
+
+import com.pulumi.Pulumi;
+import com.pulumi.core.Output;
+import com.pulumi.aws.s3.Bucket;
+
+public class App {
+    public static void main(String[] args) {
+        Pulumi.run(ctx -> {
+            var bucket = new Bucket("my-bucket");
+            ctx.export("bucketName", bucket.bucket());
+        });
+    }
+}
